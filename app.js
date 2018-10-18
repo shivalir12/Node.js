579cf7680e6185a7498d77df8b6525fc661f8933
Shivali Ranka <36067722+shivalir12@users.noreply.github.com>
2018-10-18 16:17:06 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
- console.log('Starting App');
- const fs=require('fs');
- const os= require('os');
- var user=os.userInfo();
-console.log(user);
- /*fs.appendFile('greeting.txt','Hello World!',function(err)
- {
-     if(err)
-     {
-         console.log('Unable to write to file'); 
-     }
- }
- );*/
+ console.log('Starting app.js');
+
+ const fs = require('fs');
+ const os = require('os');
+ const notes = require('./notes.js');
+ var res = notes.addNote();
+ console.log('Result:',notes.add(9,-2));
+ /*var user = os.userInfo();
+
+ fs.appendFile('greeting.txt',`Hello ${user.username}! You are ${notes.age}`);
+ */
